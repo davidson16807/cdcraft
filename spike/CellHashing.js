09e693d70bdca3c1e6ec7acca12852ac855fe51c
Carl Davidson <davids14@illinoisalumni.org>
2021-10-23 15:55:53 -0600

    objects are inferred from arrows (so beautiful 🥲)

---
+++ spike/CellHashing.js
@@ -0,0 +1,10 @@
+
+// function test(f){
+//     for(let y=-2; y<=2; y++){
+//         let line = '';
+//         for(let x=-2; x<=2; x++){
+//             line += f(x,y)+',';
+//         }
+//         console.log(line);
+//     }
+// }
