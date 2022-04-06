1276ad6fa755f3a9e14bbe135c237ffb2e9cab85
Carl Davidson <davids14@illinoisalumni.org>
2022-04-06 11:52:19 -0600

    organizing folders by model/view/update classification

--- scripts/models/ApplicationState.js
+++ scripts/models/ApplicationState.js
@@ -5,13 +5,13 @@
 It guarantees that for any combination of real numbered attribute values there is a valid application state.
 */
 class ApplicationState {
-    constructor(model, view, dragging, drag_state){
+    constructor(model, view, dragging, drag_state, undo_history, redo_history){
         this.diagram = model;
         this.view = view;
         this.drag_type = dragging;
         this.drag_state = drag_state;
-        // this.undo_history = undo_history;
-        // this.redo_history = redo_history;
+        this.undo_history = undo_history || [];
+        this.redo_history = redo_history || [];
     }
 }
 
