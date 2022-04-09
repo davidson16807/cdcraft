8c136a1a7307f50afd80d4324aa4090c70bc20d5
Carl Davidson <davids14@illinoisalumni.org>
2022-04-09 10:01:52 -0600

    extending copy method to Diagrams

--- scripts/models/Diagram.js
+++ scripts/models/Diagram.js
@@ -8,11 +8,22 @@ This includes all model and view state but excludes control state,
 such as the state of mouse drags or the undo/redo history itself. For control state, see `Application`.
 */
 class Diagram {
-    constructor(arrows, objects, screen_frame_store, object_selections, arrow_selections){
+    constructor(arrows, objects, screen_frame_store, arrow_selections, object_selections){
         this.arrows = arrows;
         this.objects = objects;
         this.screen_frame_store = screen_frame_store;
-        this.object_selections = object_selections || [];
         this.arrow_selections = arrow_selections || [];
+        this.object_selections = object_selections || [];
     }
+
+    copy(){
+        return new Diagram(
+            arrows.map(arrow => arrow.copy()),
+            objects.map(object => object.copy()),
+            screen_frame_store.copy(),
+            arrow_selections.map(arrow => arrow.copy()),
+            object_selections.map(object => object.copy()),
+        );
+    }
+
 }
