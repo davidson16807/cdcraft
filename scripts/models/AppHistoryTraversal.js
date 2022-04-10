41cc025a6db9d3939e3f6f5b60f5b1834fe4bc02
Carl Davidson <davids14@illinoisalumni.org>
2022-04-09 23:46:38 -0600

    implementing soft/hard undo distinction, a la sublime text

--- scripts/models/AppHistoryTraversal.js
+++ scripts/models/AppHistoryTraversal.js
@@ -6,23 +6,37 @@ function AppHistoryTraversal() {
             if (diagram != app_io.diagram) {
                 if (is_recorded) {
                     app_io.undo_history.push(app_io.diagram);
-                    app_io.redo_history = [];
+                    if (diagram.arrows != app_io.diagram.arrows ||
+                        diagram.objects != app_io.diagram.objects) {
+                        // only reset redo history if change is hard
+                        app_io.redo_history = [];
+                    }
                 }
                 app_io.diagram = diagram;
             }
         },
 
-        undo: function(app_io){
-            const diagram = app_io.undo_history.pop();
-            if (diagram != null) {
+        undo: function(app_io, is_soft){
+            let is_done = false;
+            let diagram;
+            while(!is_done && app_io.undo_history.length > 0) {
+                diagram = app_io.undo_history.pop();
+                is_done = (is_soft || 
+                    diagram.arrows != app_io.diagram.arrows ||
+                    diagram.objects != app_io.diagram.objects);
                 app_io.redo_history.push(app_io.diagram);
                 app_io.diagram = diagram;
             }
         },
 
-        redo: function(app_io){
-            const diagram = app_io.redo_history.pop();
-            if (diagram != null) {
+        redo: function(app_io, is_soft){
+            let is_done = false;
+            let diagram;
+            while(!is_done && app_io.redo_history.length > 0) {
+                diagram = app_io.redo_history.pop();
+                is_done = (is_soft || 
+                    diagram.arrows != app_io.diagram.arrows ||
+                    diagram.objects != app_io.diagram.objects);
                 app_io.undo_history.push(app_io.diagram);
                 app_io.diagram = diagram;
             }
