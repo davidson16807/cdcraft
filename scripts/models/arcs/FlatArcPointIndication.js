9790dedf8ceffe0b2e3e70795fcf1ad6343b0475
Carl Davidson <diadophus@gmail.com>
2023-01-26 00:05:11 -0700

    fixing bug where domain of 2-arrow is not the arc midpoint

--- scripts/models/arcs/FlatArcPointIndication.js
+++ scripts/models/arcs/FlatArcPointIndication.js
@@ -8,9 +8,9 @@ function FlatArcPointIndication(
         /*
         Returns a single position vector that visually indicates the arc to the user.
         */
-        point: (arc) => 
-            sampler_arc_properties.position(
-                user_arcs_and_sampler_arcs.flat_arc_to_sampler_arc(arc),
-                representative_distance_along_arc),
+        point: (arc) => {
+            const sampler_arc = user_arcs_and_sampler_arcs.flat_arc_to_sampler_arc(arc);
+            return sampler_arc_properties.position(sampler_arc, representative_distance_along_arc * sampler_arc.length_clockwise)
+        },
     };
 }
