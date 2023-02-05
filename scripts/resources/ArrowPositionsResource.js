e4d65598b6c3fa8fb14e6eadb6865f241b197d68
Carl Davidson <diadophus@gmail.com>
2023-02-04 21:26:11 -0700

    simplifying logic responsible for selection drags

--- scripts/resources/ArrowPositionsResource.js
+++ scripts/resources/ArrowPositionsResource.js
@@ -26,22 +26,38 @@ function ArrowPositionsResource(node_hashing, user_arcs_and_stored_arcs){
         put: function(arrows, position_map) {
             const updated_arrows = [];
             for(let arrow of arrows){
+                /*
+                Dragging objects and will affect objects and arrows whose positions or nodes share the same cells,
+                so both keys and values of a resource window should be stored in a way that is agnostic between arrows and objects.
+                The only way to satisfy this constraint is to represent positions 
+                as they would appear in `StoredArc`s (what we call a "stored position").
+                Sources and targets of arrows can be dragged by the user, 
+                and dragging these should allow users to change the directionality of loops represented by `target_offset_id`,
+                so behavior must be expressed to account for this feature. 
+                More specifically, if a user selects a loop without dragging it, the loop should not change its direction,
+                meaning that `target_offset_id` would be used as input to dragging behavior,
+                if at the very least to account for behavior where no drag occurs.
+                `target_offset_id` is a property that is specific to individual arrows,
+                and as mentioned above, resource windows (i.e. `position_map`) must represent positions in an arrow-neutral way, 
+                using stored positions, so `target_offset_id` cannot be represented somehow using the window of the resource.
+
+                The approach we take here is to use values from `position_map` to modify the `StoredArc`s of arrows,
+                then convert the `StoredArc` to and from a `UserArc`.
+                It is important to note here that the maps to and from `StoredArc` and `UserArc` are not bijective - 
+                apply these maps allow the `target_offset_id` of the initial `StoredArc`
+                to be properly considered when determining the position of the `UserArc`,
+                andl by extension the `target_offset_id` of the final `StoredArc`.
+                */
                 const old_stored = arrow.arc;
-                const old_users = user_arcs_and_stored_arcs.stored_arc_to_user_arc(old_stored);
                 const source_hash = node_hashing.hash(old_stored.source);
                 const target_hash = node_hashing.hash(old_stored.target);
-                const new_users = new UserArc(
-                    old_users.source.with({
-                        position: position_map[source_hash] != null? 
-                                    old_stored.target_offset_id.mul(-0.015).add(position_map[source_hash]) 
-                                  : old_users.source.position
-                    }),
-                    old_users.target.with({
-                        position: position_map[target_hash] != null? 
-                                    old_stored.target_offset_id.mul( 0.015).add(position_map[target_hash]) 
-                                  : old_users.target.position
-                    }),
-                    old_users.min_length_clockwise);
+                const new_users = 
+                    user_arcs_and_stored_arcs.stored_arc_to_user_arc(
+                        old_stored.with({
+                            source: old_stored.source.with({position: position_map[source_hash] || old_stored.source.position}),
+                            target: old_stored.target.with({position: position_map[target_hash] || old_stored.target.position}),
+                        })
+                    );
                 const new_stored = user_arcs_and_stored_arcs.user_arc_to_stored_arc(new_users, old_stored.target_offset_id);
                 updated_arrows.push(arrow.with({arc:new_stored}));
             }
