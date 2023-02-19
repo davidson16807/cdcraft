d80bb75bb26416ff9ac0b905fb89bcb97f86dd27
Carl Davidson <diadophus@gmail.com>
2023-02-18 23:08:41 -0700

    rename: ViewEventDeferal→ViewEventDeferalCurried

--- scripts/views/ViewEventDeferralCurried.js
+++ scripts/views/ViewEventDeferralCurried.js
@@ -1,14 +1,14 @@
 'use strict';
 
 /*
-`ViewEventDeferal` returns a convenience namespace of curried functions that generate 
+`ViewEventDeferralCurried` returns a convenience namespace of curried functions that generate 
 standardized event callbacks for `*View` namespaces.
 If an event occurs, the event callbacks will fire off a callback 
 with information about the object and drawing.
 This is done so that functions in `*View` namespaces can defer side effects 
 to callbacks provided by higher levels of object composition outside the `*View` class.
 */
-const ViewEventDeferal = 
+const ViewEventDeferralCurried = 
     () => (drawing, app, dom) => 
         ({
 
